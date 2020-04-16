a0b35fc5c9b51f69648d31d987b1fa94496d1b7f
Nico Florez <nicolascorrea1989@hotmail.com>
2020-04-16 11:00:36 -0500

    Inicio de api para pruebas técnicas El Tiempo Nicolás Flórez

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict'
+var mongoose = require('mongoose');
+var app = require('./app');
+var port = 3901;
+mongoose.Promise = global.Promise;
+mongoose.set('useFindAndModify',false);
+mongoose.connect('mongodb+srv://nflorez:Niko32@cluster0-osap7.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser:true})
+.then(() =>{
+    console.log('Ya nos conectamos a BD Mongo Cloud ! =)');
+
+    app.listen(port,()=>{
+        console.log('Servidor corriendo en http://localhost:'+port);
+    });
+}).catch(err => console.log('Algo salió mal veamos el error ' + err));
