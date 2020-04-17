26846d50cf7fb9f804c3f7293dbb2f99811e1189
Nico Florez <nicolascorrea1989@hotmail.com>
2020-04-17 11:37:31 -0500

    Inserción de nuevo componente

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 app.use('/eltiempo',produc_routes);
 
 module.exports = app;
